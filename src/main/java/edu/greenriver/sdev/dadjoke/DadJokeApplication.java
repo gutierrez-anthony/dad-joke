deb338e06f87608691992ae42a1f45dd7801a2dd
gutierrez-anthony <gutierrez.anthony@student.greenriver.edu>
2024-01-18 13:46:49 -0800

    New  dad joke files created.

---
+++ src/main/java/edu/greenriver/sdev/dadjoke/DadJokeApplication.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.dadjoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadJokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadJokeApplication.class, args);
+    }
+
+}
