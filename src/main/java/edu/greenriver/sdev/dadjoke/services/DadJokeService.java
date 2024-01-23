0afe194f72b566c552d7de79b54e03bf79fa5a93
gutierrez-anthony <gutierrez.anthony@student.greenriver.edu>
2024-01-23 13:40:08 -0800

    Update and delete method added to service.

---
+++ src/main/java/edu/greenriver/sdev/dadjoke/services/DadJokeService.java
@@ -0,0 +1,36 @@
+package edu.greenriver.sdev.dadjoke.services;
+
+import edu.greenriver.sdev.dadjoke.databases.DadJokeRepository;
+import edu.greenriver.sdev.dadjoke.models.DadJoke;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DadJokeService {
+
+    private DadJokeRepository repository;
+
+    public DadJokeService(DadJokeRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<DadJoke> getAll() {
+        return repository.findAll();
+    }
+
+    public void addDadJoke(DadJoke dadJoke) {
+        repository.save(dadJoke);
+    }
+
+    public void updateJoke(int id, String jokeText) {
+        DadJoke currentDadJoke = repository.findById(id).orElseThrow();
+
+        currentDadJoke.setJokeText(jokeText);
+        repository.save(currentDadJoke);
+    }
+
+    public void deleteDadJoke(int id) {
+        repository.delete(repository.findById(id).orElseThrow());
+    }
+}
